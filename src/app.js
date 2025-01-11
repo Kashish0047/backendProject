c2bb1a33fce0ea13965e24743f801ba611036aca
Kashish <kashishdev0786@gmail.com>
2025-01-11 15:19:00 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors"
 import cookieParser from "cookie-parser";
+//routes import
+import userRouter from './routes/user.routes.js'
 
 const app = express();
 //cross origin resourse sharing only it's frontend can commmunicate with it's backend no other resource can communicate
@@ -10,13 +12,16 @@ app.use(cors({
 }))
 //configurations
 //limit jitni set kroge utna hi json aayega hum unlimited nhi kr skte vrna system crash ho jaayega
-app.use(express.json({limit: "16kb"}))
+app.use(express.json({limit: "16kb"}));
 //urls me %20 + hote hai unhe smjhne ke liye urlencoded use kia hai and extended matlab ki hum objects me further objects create kar skte hai
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 // files images ke liye configure taki hum images files use kr ske
-app.use(express.static("public"))
+app.use(express.static("public"));
 // taki hum user ke browser ki cookies access kr ske
 app.use(cookieParser())
 
+
+//routes declaration
+app.use("/api/v1/users",userRouter);
 
 export {app}
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import {v2 as cloudinary} from "cloudinary"// helps in image uploading using multer
+import fs from "fs" // helps to read,remove,write files
+
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localFile) => {
+     try{
+        if(!localFile) return null;
+        //upload
+        const response = await cloudinary.uploader.upload(localFile,{
+            resource_type: "auto"
+        })
+        // file has been uploaded successfully
+        console.log("file has been uploaded on cloudinary", response.url);
+        return response;
+
+     } catch (error){
+        fs.unlinkSync(localFile) // remove the locally saved temporary file as the operation got failed
+        return null
+     }
+}
+
+export {uploadOnCloudinary};
